7cc7499098afda3d60be3e36e596b745e5779526
Dan <dan@dancodes.online>
2019-08-19 19:35:22 +0200

    double quotes

--- src/main/utils/handlers/database.js
+++ src/main/utils/handlers/database.js
@@ -1,15 +1,15 @@
-module.exports.start = function (options) {
-  const log = require('../../tests/logger');
-  const Enmap = require('enmap');
-  const EnmapMongo = require('enmap-mongo');
-	 const db = new Enmap({
+module.exports.start = function(options) {
+  const log = require("../../tests/logger");
+  const Enmap = require("enmap");
+  const EnmapMongo = require("enmap-mongo");
+  const db = new Enmap({
     provider: new EnmapMongo({
       name: options.name,
-      url: options.mongo,
-    }),
+      url: options.mongo
+    })
   });
 
-  db.defer.then(() => db.set('hello', 'world') && log('database'));
+  db.defer.then(() => db.set("hello", "world") && log("database"));
   module.exports = db;
 
   return db;
